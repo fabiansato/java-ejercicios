a968cf17aff0cb2f8b54157512c19a1cb56f813c
Fabian Sato <40687901+fabiansato@users.noreply.github.com>
2022-01-23 09:30:21 -0300

    ejercicios JAVA
    ejercicios JAVA

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a0/d05d7b662370001c17b3ba238cd85ee3
@@ -0,0 +1,5 @@
+package holamundo;
+
+public class holamundo {
+
+}
---
+++ A00_Holamundo.java
@@ -0,0 +1,5 @@
+public class A00_Holamundo {
+	 public static void main(String[] args) {
+	        System.out.println("Hola Mundo"); 
+	    }
+}
